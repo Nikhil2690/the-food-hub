02bf361e3ed0f6c96f7d64965db9bb60ed6c23f9
Nikhil2690 <nikhiladwani3@gmail.com>
2025-02-25 17:11:16 +0530

    Removed the Repeated Cors Middleware

--- backend/index.js
+++ backend/index.js
@@ -23,15 +23,6 @@ app.use(cors({
 }));
 
 
-app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
